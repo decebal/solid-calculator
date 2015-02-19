eaad00e3be612e469dd91d77d68a61f1065bfc22
decebal <decebal.dobrica@gmail.com>
2015-02-19 12:16:33 +0200

    return to stable version

--- src/Controller/Calculator.php
+++ src/Controller/Calculator.php
@@ -15,29 +15,25 @@ class Calculator
         $memberA = '';
 
         $operations = $input->iterator->getOperationsByPriority();
-        $graph = array();
+
         while($inputIterator->valid()) {
             if ($input->isOperator($inputIterator->current())) {
                 $operator = $inputIterator->current();
 
                 $inputIterator->next();
-                $memberB = $inputIterator->key();
+                $memberB = $inputIterator->current();
 
                 $operationInstance = new $operations[$operator]();
-                $graph[$memberA][$memberB] = $operationInstance->getInversePriority();
-                $graph[$memberB][$memberA] = $operationInstance->getInversePriority();
 
-                $memberA = $inputIterator->key();
+                $memberA = $operationInstance->compute($memberA, $memberB);
             } else {
-                $memberA = $inputIterator->key();
+                $memberA = $inputIterator->current();
             }
 
             $inputIterator->next();
         }
-        $g = new Graph($graph);
-        
 
-        return $result;
+        return $memberA;
     }
 
 }
