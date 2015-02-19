7dd3066465e894965ad081996a7243fd6b5f4d31
Decebal D <decebal1988@gmail.com>
2015-02-19 03:51:50 +0200

    add graph for computing precedence

--- src/Controller/Calculator.php
+++ src/Controller/Calculator.php
@@ -1,5 +1,6 @@
 <?php namespace App\Controller;
 
+use App\Models\Graph;
 use App\Models\Input;
 use App\Models\OperationIterator;
 
@@ -14,7 +15,7 @@ class Calculator
         $memberA = '';
 
         $operations = $input->iterator->getOperationsByPriority();
-        $queue = new \SplPriorityQueue();
+        $graph = array();
         while($inputIterator->valid()) {
             if ($input->isOperator($inputIterator->current())) {
                 $operator = $inputIterator->current();
@@ -23,14 +24,8 @@ class Calculator
                 $memberB = $inputIterator->key();
 
                 $operationInstance = new $operations[$operator]();
-                $queue->insert(
-                    array(
-                        'operationInstance' => $operationInstance,
-                        'firstMemberKey' => $memberA,
-                        'secondMemberKey' => $memberB
-                    ),
-                    $operationInstance->getInversePriority()
-                );
+                $graph[$memberA][$memberB] = $operationInstance->getInversePriority();
+                $graph[$memberB][$memberA] = $operationInstance->getInversePriority();
 
                 $memberA = $inputIterator->key();
             } else {
@@ -39,40 +34,8 @@ class Calculator
 
             $inputIterator->next();
         }
-
-        $queue->top();
-        $resetPair = array();
-        $result = 0;
-        while (!$queue->isEmpty()) {
-            $value = $queue->extract();
-
-            $operationInstance = new $value['operationInstance']();
-
-            if (isset($resetPair[$value['firstMemberKey']])) {
-                $inputArray[$value['firstMemberKey']] = $result;
-                unset($resetPair[$value['firstMemberKey']]);
-                foreach ($resetPair as $resetKey => $resetValue) {
-                    unset($inputArray[$resetKey]);
-                }
-                unset($resetValue);
-                $resetPair = array();
-            } elseif (isset($resetPair[$value['secondMemberKey']])) {
-                $inputArray[$value['secondMemberKey']] = $result;
-                unset($resetPair[$value['secondMemberKey']]);
-                foreach ($resetPair as $resetKey => $resetValue) {
-                    unset($inputArray[$resetKey]);
-                }
-                unset($resetValue);
-                $resetPair = array();
-            }
-            $result = $operationInstance->compute(
-                $inputArray[$value['firstMemberKey']],
-                $inputArray[$value['secondMemberKey']]
-            );
-
-            $resetPair[$value['firstMemberKey']] = 1;
-            $resetPair[$value['secondMemberKey']] = 1;
-        }
+        $g = new Graph($graph);
+        
 
         return $result;
     }
